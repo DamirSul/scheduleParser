2c0aeee555ac0ea0356a534d8268c5dc45ad413f
Damir Suleymenov <suleimenoff1@yandex.ru>
2024-02-11 22:54:02 +0600

    first version parser

---
+++ parser.py
@@ -0,0 +1,41 @@
+import telebot
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+import time
+
+bot = telebot.TeleBot('6715780052:AAHLF3BSwdfTIrOXfPlADPLJ5Qup6at5xr4')
+driver = webdriver.Chrome()
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, "Привет, я рад тебя видеть. У меня ты сможешь получить расписание своей группы.")
+
+@bot.message_handler(commands=['search_timetable'])
+def search_timetable(message):
+    msg = bot.send_message(message.chat.id, "Введите группу, расписание которой вы хотите найти.")
+    bot.register_next_step_handler(msg, search)
+
+@bot.message_handler(content_types=['text'])
+def text(message):
+    bot.send_message(message.chat.id, "Уточни, что ты хотел?")
+def search(message):
+    bot.send_message(message.chat.id, "Начинаю поиск")
+    driver.get("https://umu.sibadi.org/WebApp/#/Rasp/List")
+    time.sleep(2)
+    our_cell = driver.find_element(By.ID, "input-67")  # находим поле с вводом
+    user_input = message.text  # перменная содержащая в себе текст из бота тг
+    time.sleep(2)
+    our_cell.send_keys(user_input)
+    time.sleep(3)
+    group = driver.find_element(By.CLASS_NAME, "text-start")  # поле с выводимыми группами
+    time.sleep(2)
+    group.click()  # заходим на группу
+    time.sleep(7)
+    driver.quit()
+
+
+bot.polling()
